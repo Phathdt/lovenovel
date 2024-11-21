f1eb9a80c5d6a9816cd34bdf03e5e29eb697bc4c
Phathdt <phathdt379@gmail.com>
2024-11-21 23:03:46 +0700

    init

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,32 @@
+import { LoggerModule } from 'nestjs-pino';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import pretty from 'pino-pretty';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [],
+      envFilePath: ['.env'],
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            colorize: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,19 +3,41 @@
  * This is only a minimal backend to get started.
  */
 
+import { Logger as PinoLogger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { ZodValidationPipe } from 'nestjs-zod';
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app/app.module';
+import { ResponseExceptionFilter } from './interceptors/response-exception.filter';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { TraceIdInterceptor } from './interceptors/trace-id.interceptor';
+import { ZodValidationExceptionFilter } from './interceptors/zod-validation-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors: true,
+  });
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  app.useGlobalFilters(
+    new ResponseExceptionFilter(),
+    new ZodValidationExceptionFilter()
+  );
+  app.useGlobalInterceptors(
+    new LoggerErrorInterceptor(),
+    new TraceIdInterceptor(),
+    new ResponseInterceptor()
+  );
+
+  app.useLogger(app.get(PinoLogger));
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
